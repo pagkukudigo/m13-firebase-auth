5fdb2cb754f4c4223d6db056f61413e40d6749de
pagkukudigo <saralyns@uw.edu>
2016-10-25 10:14:05 -0700

    participation point + saved slick

--- exercise-1/js/authenticate.js
+++ exercise-1/js/authenticate.js
@@ -2,25 +2,50 @@
 $(function() {
 
     // Initialize Firebase
-
+    var config = {
+        apiKey: "AIzaSyCJb6FKtaxQ7TECoFvU4nkq1N9k-lnyROk",
+        authDomain: "slick-328a5.firebaseapp.com",
+        databaseURL: "https://slick-328a5.firebaseio.com",
+        storageBucket: "slick-328a5.appspot.com",
+        messagingSenderId: "72335202957"
+    };
+    firebase.initializeApp(config);
+    
     // Sign Up: Function to create account on firebase, then redirect to index.html
     var signUp = function() {
         // Get email, password, and display name
-
+        var email = $('#email').val();
+        var password = $('#password').val();
+        var displayName = $('#display-name').val();
+        
+        console.log('email: ' + email + ', password: ' + password + ', display name: ' + displayName);
 
         // Create user, then set the user's display name
-
-                // Set display name
-
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+           user.updateProfile({
+               displayName: displayName,
+           }).then(function(){
+               window.location = '/';
+           });
+        }).catch(function(error){
+               alert(error.message);
+        });
     };
 
     // SignIn: Function to authenticate on Firebase, then redirect to index.html
     var signIn = function() {
         // Get email and password
-
-
+        var email = $('#email').val();
+        var password = $('#password').val();
+        console.log('email: ' + email + ', password: ' + password);
+        
+        
         // Authenticate using email and password, then redirect
-
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+            window.location = '/';
+        }).catch(function(error) {
+            alert(error);
+        });
     };
 
     // Sign out: Function to log a user out of firebase
@@ -32,8 +57,15 @@ $(function() {
     };
 
     // Assign event lister to form submission
-
-
+     $('form').on('submit', function(event) {
+        event.preventDefault();
+        var formId = $(this).attr('id');
+        if (formId == 'sign-up') {
+            signUp();
+        } else if (formId == 'sign-in') {
+            signIn();
+        }
+    });
 
     // Assign click event to logout button
 
